c64ee860e13bbdbc981b135f3dcf11a244660612
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-16 20:14:49 -0300

    completei ClienteRequest e ClienteResponse

--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteRequest.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteRequest.java
@@ -1,4 +1,14 @@
 package com.chimionato.cadastrocliente.application.api;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
 }
--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteResponse.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteResponse.java
@@ -1,4 +1,10 @@
 package com.chimionato.cadastrocliente.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class ClienteResponse {
+    private UUID idCliente;
 }
