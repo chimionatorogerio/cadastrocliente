3702f2e27a840d0ff83868c7148f3e2052b8164d
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-16 18:27:14 -0300

    criei ClienteController so com logs - teste Postman ok

---
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteController.java
@@ -0,0 +1,15 @@
+package com.chimionato.cadastrocliente.application.api;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class ClienteController implements ClienteAPI {
+    @Override
+    public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+        log.info("[inicia  ] x ClienteController - postCliente");
+        log.info("[finaliza] x ClienteController - postCliente");
+        return null;
+    }
+}
