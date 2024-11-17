b79489a8df55443af613d5eaa66fa5f1df570f5a
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-16 21:52:22 -0300

    criei interface ClienteRepository com método

---
+++ src/main/java/com/chimionato/cadastrocliente/application/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.chimionato.cadastrocliente.application.repository;
+
+import com.chimionato.cadastrocliente.domain.Cliente;
+
+public interface ClienteRepository {
+    Cliente postCliente(Cliente cliente);
+}
--- src/main/java/com/chimionato/cadastrocliente/application/service/ClienteApplicationService.java
+++ src/main/java/com/chimionato/cadastrocliente/application/service/ClienteApplicationService.java
@@ -2,6 +2,7 @@ package com.chimionato.cadastrocliente.application.service;
 
 import com.chimionato.cadastrocliente.application.api.ClienteRequest;
 import com.chimionato.cadastrocliente.application.api.ClienteResponse;
+import com.chimionato.cadastrocliente.application.repository.ClienteRepository;
 import com.chimionato.cadastrocliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,7 +17,7 @@ public class ClienteApplicationService implements ClienteService {
     @Override
     public ClienteResponse postCliente(ClienteRequest clienteRequest) {
         log.info("[inicia  ] @ ClienteApplicationService - criaCliente");
-        Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
+        Cliente cliente = clienteRepository.postCliente(new Cliente(clienteRequest));
         log.info("[finaliza] @ ClienteApplicationService - criaCliente");
         return ClienteResponse.builder()
                 .idCliente(cliente.getIdCliente())
