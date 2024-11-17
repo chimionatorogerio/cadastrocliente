24ec53f24373facf3e9b71a31cafae76663e4648
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 11:48:33 -0300

    ClienteController: Primeiro coloquei só os logs, testei no Postman e deu 200. Depois coloquei o método e o retorno

--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteController.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Log4j2
 @RequiredArgsConstructor
@@ -17,5 +19,13 @@ public class ClienteController implements ClienteAPI {
         ClienteResponse clienteCriado = clienteService.postCliente(clienteRequest);
         log.info("[finaliza] x ClienteController - postCliente");
         return clienteCriado;
+    }
+
+    @Override
+    public List<ClienteListResponse> getTodosClientes() {
+        log.info("[inicia  ] y ClienteController - getTodosClientes");
+        List<ClienteListResponse> clientes = clienteService.getTodosClientes();
+        log.info("[finaliza] y ClienteController - getTodosClientes");
+        return clientes;
     }
 }
