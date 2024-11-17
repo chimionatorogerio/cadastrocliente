064cc7d3a9458ce07dbfbbef3703b198f6fc0195
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 16:44:14 -0300

    ClienteListResponse: incluí método converte e construtor

--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteListResponse.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteListResponse.java
@@ -1,11 +1,26 @@
 package com.chimionato.cadastrocliente.application.api;
 
+import com.chimionato.cadastrocliente.domain.Cliente;
 import lombok.Value;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class ClienteListResponse {
     private UUID idCliente;
     private String nomeCompleto;
     private String email;
+
+    public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+    private ClienteListResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.email = cliente.getEmail();
+    }
 }
