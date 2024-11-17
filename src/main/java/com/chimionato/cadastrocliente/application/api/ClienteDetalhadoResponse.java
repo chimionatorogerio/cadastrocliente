42b03370f8ede25d1eb654e8eaba53478825d51c
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 18:53:51 -0300

    criei ClienteDetalhadoResponse com os campos

---
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteDetalhadoResponse.java
@@ -0,0 +1,10 @@
+package com.chimionato.cadastrocliente.application.api;
+
+import java.util.UUID;
+
+public class ClienteDetalhadoResponse {
+    private UUID idCliente;
+    private String nomeCompleto;
+    private String email;
+    private String CPF;
+}
