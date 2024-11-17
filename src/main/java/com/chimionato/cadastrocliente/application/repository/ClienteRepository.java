8df53b83cfe0290cc3ef203b505e7311925a4e16
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 16:49:24 -0300

    ClienteRepository: incluí método

--- src/main/java/com/chimionato/cadastrocliente/application/repository/ClienteRepository.java
+++ src/main/java/com/chimionato/cadastrocliente/application/repository/ClienteRepository.java
@@ -2,6 +2,9 @@ package com.chimionato.cadastrocliente.application.repository;
 
 import com.chimionato.cadastrocliente.domain.Cliente;
 
+import java.util.List;
+
 public interface ClienteRepository {
     Cliente postCliente(Cliente cliente);
+    List<Cliente> getTodosClientes();
 }
