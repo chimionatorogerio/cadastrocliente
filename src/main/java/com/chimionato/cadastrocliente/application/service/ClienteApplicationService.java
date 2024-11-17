907ed04e4e93b8e82dee562921b713836d89f1b6
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 16:14:10 -0300

    ClienteApplicationService: criei getTodosClientes só com logs - teste Postman ok - em seguida incluí método e retorno

--- src/main/java/com/chimionato/cadastrocliente/application/service/ClienteApplicationService.java
+++ src/main/java/com/chimionato/cadastrocliente/application/service/ClienteApplicationService.java
@@ -1,5 +1,6 @@
 package com.chimionato.cadastrocliente.application.service;
 
+import com.chimionato.cadastrocliente.application.api.ClienteListResponse;
 import com.chimionato.cadastrocliente.application.api.ClienteRequest;
 import com.chimionato.cadastrocliente.application.api.ClienteResponse;
 import com.chimionato.cadastrocliente.application.repository.ClienteRepository;
@@ -7,6 +8,8 @@ import com.chimionato.cadastrocliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
@@ -16,11 +19,19 @@ public class ClienteApplicationService implements ClienteService {
 
     @Override
     public ClienteResponse postCliente(ClienteRequest clienteRequest) {
-        log.info("[inicia  ] @ ClienteApplicationService - criaCliente");
+        log.info("[inicia  ] @ ClienteApplicationService - postCliente");
         Cliente cliente = clienteRepository.postCliente(new Cliente(clienteRequest));
-        log.info("[finaliza] @ ClienteApplicationService - criaCliente");
+        log.info("[finaliza] @ ClienteApplicationService - postCliente");
         return ClienteResponse.builder()
                 .idCliente(cliente.getIdCliente())
                 .build();
+    }
+
+    @Override
+    public List<ClienteListResponse> getTodosClientes() {
+        log.info("[inicia  ] & ClienteApplicationService - getTodosClientes");
+        List<Cliente> clientes = clienteRepository.getTodosClientes();
+        log.info("[finaliza] & ClienteApplicationService - getTodosClientes");
+        return ClienteListResponse.converte(clientes);
     }
 }
