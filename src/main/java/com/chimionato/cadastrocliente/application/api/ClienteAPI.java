282ba5cc755a86670ee12eda7c47bcee74832ccc
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 18:39:20 -0300

    INICIA A IMPLANTAÇÃO DO getUmClientePeloId - inseri método no ClienteAPI

--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteAPI.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteAPI.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/cliente")
@@ -17,5 +18,9 @@ public interface ClienteAPI {
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     List<ClienteListResponse> getTodosClientes();
+
+    @GetMapping(value = "/{idCliente}")
+    @ResponseStatus(code = HttpStatus.OK)
+    ClienteDetalhadoResponse getUmClientePeloId(@PathVariable UUID idCliente);
 
 }
