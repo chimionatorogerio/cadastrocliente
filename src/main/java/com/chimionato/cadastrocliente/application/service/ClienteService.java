f314df4305f48cf06eacaa4a8f9afc9e2245d6fa
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-17 21:02:04 -0300

    ClienteService: inseri método

--- src/main/java/com/chimionato/cadastrocliente/application/service/ClienteService.java
+++ src/main/java/com/chimionato/cadastrocliente/application/service/ClienteService.java
@@ -1,12 +1,15 @@
 package com.chimionato.cadastrocliente.application.service;
 
+import com.chimionato.cadastrocliente.application.api.ClienteDetalhadoResponse;
 import com.chimionato.cadastrocliente.application.api.ClienteListResponse;
 import com.chimionato.cadastrocliente.application.api.ClienteRequest;
 import com.chimionato.cadastrocliente.application.api.ClienteResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ClienteService {
     ClienteResponse postCliente(ClienteRequest clienteRequest);
     List<ClienteListResponse> getTodosClientes();
+    ClienteDetalhadoResponse getUmClientePeloId(UUID idCliente);
 }
