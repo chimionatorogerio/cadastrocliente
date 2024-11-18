2736d303ac640a1070ce76a266e88e891ab3bbb4
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-18 09:04:25 -0300

    ClienteDetalhadoResponse: criei construtor

--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteDetalhadoResponse.java
@@ -1,5 +1,7 @@
 package com.chimionato.cadastrocliente.application.api;
 
+import com.chimionato.cadastrocliente.domain.Cliente;
+
 import java.util.UUID;
 
 public class ClienteDetalhadoResponse {
@@ -7,4 +9,10 @@ public class ClienteDetalhadoResponse {
     private String nomeCompleto;
     private String email;
     private String CPF;
+
+    public ClienteDetalhadoResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.email = cliente.getEmail();
+    }
 }
