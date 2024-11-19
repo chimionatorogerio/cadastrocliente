11b06c7b1f954b1d985492b2e7a0214d5a39b542
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-19 12:03:24 -0300

    ClienteAlteracaoRequest: incluí @Value

--- src/main/java/com/chimionato/cadastrocliente/application/api/ClienteAlteracaoRequest.java
+++ src/main/java/com/chimionato/cadastrocliente/application/api/ClienteAlteracaoRequest.java
@@ -1,7 +1,9 @@
 package com.chimionato.cadastrocliente.application.api;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
 
+@Value
 public class ClienteAlteracaoRequest {
     @NotBlank
     private String nomeCompleto;
